404202c001a41421d9e94b78539a62fad07b544b
Devon <devonlumsden@mail.com>
2021-02-12 19:46:25 +1300

    create logoutAccount()"

--- bot.py
+++ bot.py
@@ -19,6 +19,21 @@ def getAccountLoginDetails(detail):
     else:
         return ("")
 
+def logoutAccount(browser):
+    #Profile Button Menu
+    profile_menu_button = browser.find_element_by_css_selector(
+        "span[class='_2dbep qNELH']"
+    )
+    profile_menu_button.click()
+    sleep(1)
+
+    logout_button = browser.find_element_by_xpath("//div[@class='-qQT3']")
+    logout_button.click()
+
+    sleep(10)
+    browser.close()
+
+
 def main(username, password):
     # Load Firefox WebDriver and open instagram login page
     browser = webdriver.Firefox()
@@ -42,14 +57,7 @@ def main(username, password):
     notifications_settings_dialog = browser.find_element_by_xpath("//button[@class='aOOlW   HoLwm ']")
     notifications_settings_dialog.click()
 
-    #Profile Button Menu
-    profile_menu_button = browser.find_element_by_css_selector("span[class='_2dbep qNELH']")
-    profile_menu_button.click()
-    sleep(1)
 
-    #Logout
-    logout_button = browser.find_element_by_xpath("//div[@class='-qQT3']")
-    logout_button.click()
 
-    sleep(10)
-    browser.close()
+    logoutAccount(browser)
+    
