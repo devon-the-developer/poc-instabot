12050c2675e1edf99141374a1aa0880d05343041
Devon <devonlumsden@mail.com>
2021-02-12 21:06:11 +1300

    make gotoProfile work with different users

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from time import sleep
 from selenium import webdriver
 
+accountusername = ''
+
 def getAccountLoginDetails(detail):
     # Load instagram account login from .env file
     dotenv_path = join(dirname(__file__), '.env')
@@ -38,41 +40,39 @@ def gotoHome(browser):
     browser.get('https://www.instagram.com' + homelink)
 
 def gotoProfile(browser):
-    profilelink = '/mereleenz/'
+    profilelink = '/'+ accountusername + '/'
     browser.get('https://www.instagram.com' + profilelink)
 
 
 def main(username, password):
+    #set accountusername for use elsewhere 
+    global accountusername 
+    accountusername = username
+
     # Load Firefox WebDriver and open instagram login page
     browser = webdriver.Firefox()
     browser.implicitly_wait(10)
-
     browser.get('https://www.instagram.com')
 
     username_input = browser.find_element_by_css_selector("input[name='username']")
     password_input = browser.find_element_by_css_selector("input[name='password']")
-
     username_input.send_keys(username)
     password_input.send_keys(password)
     sleep(5)
 
     login_button = browser.find_element_by_xpath("//button[@type='submit']")
     login_button.click()
-
     sleep(10)
 
     #Worth creating a variable that has all available options
     notifications_settings_dialog = browser.find_element_by_xpath("//button[@class='aOOlW   HoLwm ']")
     notifications_settings_dialog.click()
-
     sleep(1)
 
     gotoProfile(browser)
-
     sleep(3)
 
     gotoHome(browser)
-    
     sleep(10)
 
     logoutAccount(browser)
